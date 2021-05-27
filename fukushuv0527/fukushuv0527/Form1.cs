62c68a873a22a702ad5ab70942620e421671c2f0
A601-18 <tnk.dat.class@gmail.com>
2021-05-27 09:42:43 +0900

    Initial commit

---
+++ fukushuv0527/fukushuv0527/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fukushuv0527
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top - 10;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left + 10;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left + 10;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top + 10;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
